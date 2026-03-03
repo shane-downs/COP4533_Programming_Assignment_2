2f1b50d01352f15be5472052a7693be3b08b267f
shankaplier <99546364+shankaplier@users.noreply.github.com>
2026-03-03 09:20:27 -0500

    Implemented LRU Cache

--- src/main.py
+++ src/main.py
@@ -11,29 +11,58 @@ class FIFOCache:
             return self.cache[key]
 
     def put(self, key, value):
-        if len(self.cache) >= self.capacity:
+        if key in self.cache:
+            pass
+        elif len(self.cache) >= self.capacity:
             self.cache.popitem(last=False)
             self.misses += 1
             self.cache[key] = value
         elif key not in self.cache:
             self.misses += 1
             self.cache[key] = value
-        elif key in self.cache:
-            pass
+
+
+    def printCache(self):
+        for key, value in self.cache.items():
+            print(key, value)
+
+class LRUCache:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+        self.misses = 0
+
+    def get(self, key):
+        if key in self.cache:
+            self.cache.move_to_end(key, True)
+            return self.cache[key]
+
+    def put(self, key, value):
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        elif len(self.cache) >= self.capacity:
+            self.cache.popitem(last=False)
+            self.misses += 1
+            self.cache[key] = value
+        elif key not in self.cache:
+            self.misses += 1
+            self.cache[key] = value
 
     def printCache(self):
         for key, value in self.cache.items():
             print(key, value)
 
 
-test = FIFOCache(5)
+test = LRUCache(3)
 test.put(1, 5)
 test.put(2, 6)
+# test.put(1, 5)
 test.put(3, 7)
-test.put(4, 8)
-test.put(5, 9)
-test.put(6, 10)
-test.put(7, 11)
+test.put(1, 5)
+# test.put(4, 8)
+# test.put(5, 9)
+# test.put(6, 10)
+# test.put(7, 11)
 print(test.printCache())
 print(test.misses)
 
